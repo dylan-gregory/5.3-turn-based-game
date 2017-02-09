1cb1dde3e07773e2c0d34c1d8c42119342e928b3
dylan-gregory <jdylangregory@gmail.com>
2017-02-09 16:49:03 -0500

    Fixed constructors, added modal

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,12 +1,28 @@
-var $ = require('jquery');
+var $ = window.$ = window.jQuery = require('jquery');
 var _ = require('underscore');
-var models = require('./models');
-
-$(function(){
-
+var models = require('./models.js');
+require('bootstrap-sass');
 
 
 
 
+  // This will reference the character that you choose - it will load up that characters data into the event that will fire when you click the play button
+  // $(whatever we want to call this thing).on('click', function(event, data){
+  //   event.preventDefault();
+  //
+  //  ***** Or we can just grab that character object -> heroes[0], etc
+  //
+  //   name = data.name
+  //   move = data.move
+  //   picture = data.picture
+  // });
+  $('.ready-play-btn').on('click', function(event, data){
+    event.preventDefault();
 
-})();
+    var enemyToFight = models.enemies.indexOf(_.random(0, 2));
+
+    $('.enemy-img').append(enemyTemplate(enemyToFight));
+    // or whatever we want to call the hero spot
+    $('.hero-img').append(heroTemplate(data));
+
+  });
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -3,6 +3,7 @@
   **/
 var $ = require('jquery');
 var _ = require('underscore');
+require('./index.js')
 
 
 // Setting up a basic character constructor
@@ -11,33 +12,38 @@ function Character(config){
 
     this.name = config.name;
     this.move = config.move;
-// they will all have 100 health points by default
-  var charDefaults = {
-    health: 100,
 
-  }
 
-  $.extend(this, charDefaults, config);
+  $.extend(this, config);
 
   this.attack = function(){
     var hit = _.random(10, 30);
     return Character.health -= hit; // might have to change Character to something else - enemy health maybe?
-  }
+  };
 
 }
 
 // Good guys constructor
 
-function Hero(){
+function Hero(config){
+  config = config || {};
+  // this.name = config.name;
+  // this.move = config.move;
+    Character.call(this, config);
 
 }
 
 // Giving them the inheritance of Character
 Hero.prototype = new Character();
 
-//Bad Guys constructor
-function Enemy(){
 
+//Bad Guys constructor
+function Enemy(config){
+  config = config || {};
+
+  Character.call(this, config);
+  // this.name = config.name;
+  // this.move = config.move;
 }
 
 // Giving them inheritance of Character as well
@@ -60,6 +66,8 @@ Enemy.prototype = new Character();
   });
 
 
+  console.log(luffy);
+
 // Listing out our not so cool bad guys
   var doFlamingo = new Enemy({
     name: "Doflamingo",
@@ -68,7 +76,7 @@ Enemy.prototype = new Character();
 
   var smoker = new Enemy({
     name: "Smoker",
-    move: "Smoke Bomb", 
+    move: "Smoke Bomb",
   });
 
   var akainu = new Enemy({
@@ -76,8 +84,19 @@ Enemy.prototype = new Character();
     move: "Volcano Meteor"
   });
 
-var goodGuys = [luffy, robin, zoro];
-var badGuys = [doFlamingo, smoker, akainu];
+var heroes = [luffy, robin, zoro];
+
+var enemies = [doFlamingo, smoker, akainu];
+
+console.log(enemies);
+
+
+module.exports = {
+  "heroes": heroes,
+  "enemies": enemies
+}
+//
+// console.log(enemies[0].name);
 
 // It would be neat to display this.name + " attacked with" + this.move
 // hit for x amount of damage
